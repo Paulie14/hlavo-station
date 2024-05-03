1d83671e3acff37f1a239e303e5eec9267c52e76
Paulie14 <pavelexner@gmail.com>
2024-05-03 08:21:12 +0200

    Add statistics to meteo data.

--- libraries/Hlavo/meteo_data.h
+++ libraries/Hlavo/meteo_data.h
@@ -11,13 +11,13 @@ class MeteoData{
     unsigned int raingauge_ticks;
 
     // RTC
-    float humidity;
-    float temperature;
+    float humidity_mean, humidity_var;
+    float temperature_mean, temperature_var;
 
-    float light;
+    float light_mean, light_var;
 
     // battery - ESP32 analog read
-    float battery_voltage;
+    float battery_voltage_mean, battery_voltage_var;
 
     static const char * delimiter;
 
@@ -28,10 +28,14 @@ class MeteoData{
           "WindDirection",
           "WindSpeedTicks",
           "RainGaugeTicks",
-          "Humidity",
-          "Temperature",
-          "Light",
-          "BatteryVoltage"
+          "Humidity_Mean",
+          "Humidity_Var",
+          "Temperature_Mean",
+          "Temperature_Var",
+          "Light_Mean",
+          "Light_Var",
+          "BatteryVoltage_Mean",
+          "BatteryVoltage_Var"
         };
         csvLine[0] = '\0'; // Initialize the CSV line as an empty string
 
@@ -50,6 +54,21 @@ class MeteoData{
         return csvLine;
     }
 
+    MeteoData()
+    {
+      datetime = DateTime(0,0,0, 0,0,0);
+
+      wind_direction = 0.0f;
+      wind_speed_ticks = 0;
+      raingauge_ticks = 0;
+
+      humidity_mean = humidity_var = 0.0f;
+      temperature_mean = temperature_var = 0.0f;
+      light_mean = light_var = 0.0f;
+
+      battery_voltage_mean = battery_voltage_var = 0.0f;
+    }
+
     // Function to convert MeteoData struct to CSV string with a custom delimiter
     char* dataToCsvLine(char* csvLine) {
 
@@ -58,15 +77,23 @@ class MeteoData{
       // sprintf(datetime, "%04d-%02d-%02d %02d:%02d:%02d%c%.2f%c%u%c%u%c%.2f%c%.2f%c%.2f\n",
       //         data.datetime.year(), data.datetime.month(), data.datetime.day(),
       //         data.datetime.hour(), data.datetime.minute(), data.datetime.second());
-      sprintf(csvLine, "%s%s%.1f%s%u%s%u%s%.2f%s%.2f%s%.0f%s%.3f\n",
+      sprintf(csvLine, "%s%s%.1f%s%u%s%u%s"
+              "%.2f%s%.2f%s" // humidity
+              "%.2f%s%.2f%s" // temperature
+              "%.0f%s%.0f%s" // light
+              "%.3f%s%.3f\n",// battery
               dt, delimiter,
               wind_direction, delimiter,
               wind_speed_ticks, delimiter,
               raingauge_ticks, delimiter,
-              humidity, delimiter,
-              temperature, delimiter,
-              light, delimiter,
-              battery_voltage);
+              humidity_mean, delimiter,
+              humidity_var, delimiter,
+              temperature_mean, delimiter,
+              temperature_var, delimiter,
+              light_mean, delimiter,
+              light_var, delimiter,
+              battery_voltage_mean, delimiter,
+              battery_voltage_var);
       return csvLine;
     }
 };
